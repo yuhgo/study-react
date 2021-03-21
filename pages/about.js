3adb08500446d44c61be195d90d6326cb1a9e394
skoto <0120koto@gmail.com>
2021-03-21 12:36:24 +0900

    ♻️ ヘッドライン部分をコンポーネント化

--- pages/about.js
+++ pages/about.js
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import { Footer } from "../components/Footer";
+import Headline from "../components/Headline";
 import Links from "../components/Links";
 import styles from "../styles/Home.module.css";
 
@@ -12,13 +13,7 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>About Page</h1>
-
-        <p className={styles.description}>
-          Get started by editing{" "}
-          <code className={styles.code}>pages/about.js</code>
-        </p>
-
+        <Headline page="about" />
         <Links />
       </main>
 
