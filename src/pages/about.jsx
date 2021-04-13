0caeabdc031670bf507bdcf19f5e6dd949f0149e
skoto <0120koto@gmail.com>
2021-04-13 16:56:46 +0900

    🏗 拡張子・ディレクトリ構成・インポート方法の変更、_appを用いた処理共通化

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,15 +1,14 @@
 import Head from "next/head";
-import { Footer } from "../components/Footer";
-import { Header } from "../components/Header";
-import { Main } from "../components/Main";
-import styles from "../styles/Home.module.css";
+import { Footer } from "src/components/Footer";
+import { Header } from "src/components/Header";
+import { Main } from "src/components/Main";
+import styles from "src/styles/Home.module.css";
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
         <title>About Page</title>
-        <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
       <Main page="about" />
