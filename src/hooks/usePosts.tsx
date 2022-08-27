f2e54d50cc1ec9f9c28ea0881621cd3484fa28f7
ゆうご <yuhgo090764@gmail.com>
2022-08-27 23:17:04 +0900

    ♻ 不要部分を修正

--- src/hooks/usePosts.tsx
+++ src/hooks/usePosts.tsx
@@ -1,21 +1,13 @@
 import useSWR from "swr";
 import { Post } from "../components/posts/type";
 
-type State = {
-  data: Post[];
-  loading: boolean;
-  error: any;
-};
-
 const fetcher = async (url: string) => {
   const res = await fetch(url);
 
   if (!res.ok) {
     throw new Error("エラーが発生したため、データの取得に失敗しました。");
   }
-  const json = await res.json();
-
-  return json;
+  return await res.json();
 };
 
 export const usePosts = () => {
