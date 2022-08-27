60d9065954e24f6847ff21ab117e2c19e6e55d08
ゆうご <yuhgo090764@gmail.com>
2022-08-27 13:49:43 +0900

    :adhesive_bandage: 不要部分を削除

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,3 @@
 module.exports = {
   extends: "next",
-  // rules: {
-  //   // For JS objects sorting
-  //   "sort-keys-custom-order/object-keys": "error",
-  //   // For TS types sorting
-  //   "sort-keys-custom-order/type-keys": "error",
-  // },
 };
