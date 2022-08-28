193fee281784fb52740561bc1e2de9397e1208db
ゆうご <yuhgo090764@gmail.com>
2022-08-28 17:46:05 +0900

    🐛 パスの変更し忘れ修正

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -2,8 +2,8 @@ import Link from "next/link";
 import classes from "src/components/Header/Header.module.css";
 
 const NAV_ITEMS = [
+  { href: "/", label: "Index" },
   { href: "/posts", label: "Posts" },
-  { href: "/about", label: "About" },
 ];
 
 export const Header = () => {
