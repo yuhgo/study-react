38e932c783814ab423e7bb1d5e24a21c8fa1d83e
ゆうご <yuhgo090764@gmail.com>
2022-08-28 17:46:52 +0900

    🔥 indexから不要なコードを削除

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,44 +1,18 @@
 import Head from "next/head";
 import styles from "src/styles/Home.module.css";
-import { Footer } from "src/components/Footer";
 import { Header } from "src/components/Header";
-import { Main } from "src/components/Main";
 import { NextPage } from "next";
 import React from "react";
 
-type IndexProps = {
-  handleClick: () => void;
-  handleDisplay: () => void;
-  isShow: boolean;
-  doubleCount: number;
-  text: string;
-  array: string[];
-  handleChange: () => void;
-  handleAdd: () => void;
-};
-
-const Index: NextPage<IndexProps> = (props) => {
+const Index: NextPage = () => {
   return (
     <div className={styles.container}>
       <Head>
-        <title>About Page</title>
+        <title>Index Page</title>
       </Head>
       <Header />
-      <button onClick={props.handleClick}>ボタン</button>
-      <button onClick={props.handleDisplay}>
-        {props.isShow ? "非表示" : "表示"}
-      </button>
-      {props.isShow ? <h2>{props.doubleCount}</h2> : null}
-      <hr />
-      <input type="text" value={props.text} onChange={props.handleChange} />
-      <button onClick={props.handleAdd}>追加</button>
-      <ol>
-        {props.array.map((item: any) => {
-          return <li key={item}>{item}</li>;
-        })}
-      </ol>
-      <Main page="about" />
-      <Footer />
+      <h1>Next.jsで学ぶReact講座</h1>
+      <p>JSONPlaceholderのAPIを色々叩いてみるよ</p>
     </div>
   );
 };
