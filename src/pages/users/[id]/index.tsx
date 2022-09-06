25aa15ac2f045bb35fc49dad3e26922595b82403
ゆうご <yuhgo090764@gmail.com>
2022-09-07 00:34:40 +0900

    ✨ ユーザー詳細ページでSSRを実践

--- src/pages/users/[id]/index.tsx
+++ src/pages/users/[id]/index.tsx
@@ -1,13 +1,39 @@
 import { Header } from "src/components/Header";
 import { UserComponent } from "../../../components/user";
+import { User } from "../../../type/type";
+import { GetServerSideProps, NextPage } from "next";
+import { SWRConfig } from "swr";
 
-const UserId = () => {
+type SSRProps = {
+  fallback: {
+    [p: string]: User;
+  };
+};
+
+const UserId = (props: SSRProps) => {
+  const { fallback } = props;
+
   return (
-    <div>
+    <SWRConfig value={{ fallback }}>
       <Header />
       <UserComponent />
-    </div>
+    </SWRConfig>
   );
 };
 
 export default UserId;
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  const { id } = ctx.query;
+  const API_URL = `https://jsonplaceholder.typicode.com/users/${id}`;
+  const res = await fetch(API_URL);
+  const userData: User = await res.json();
+
+  return {
+    props: {
+      fallback: {
+        [API_URL]: userData,
+      },
+    },
+  };
+};
