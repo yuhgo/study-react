515caffb1e7f2f78b826a78edb9626de871dc1ef
ゆうご <yuhgo090764@gmail.com>
2022-09-08 23:11:06 +0900

    ✨ コメント一覧ページでSGを実践

--- src/pages/comments/index.tsx
+++ src/pages/comments/index.tsx
@@ -1,17 +1,44 @@
 import Head from "next/head";
 import { Header } from "src/components/Header";
 import { CommentsComponent } from "../../components/comments";
+import { GetStaticProps } from "next";
+import { Comment } from "../../type/type";
+import { SWRConfig } from "swr";
 
-const Comments = () => {
+type SGProps = {
+  fallback: {
+    [p: string]: Comment[];
+  };
+};
+
+const Comments = (props: SGProps) => {
+  const { fallback } = props;
+
   return (
     <div>
       <Head>
         <title>Comments Page</title>
       </Head>
-      <Header />
-      <CommentsComponent />
+      <SWRConfig value={{ fallback }}>
+        <Header />
+        <CommentsComponent />
+      </SWRConfig>
     </div>
   );
 };
 
 export default Comments;
+
+export const getStaticProps: GetStaticProps = async () => {
+  const COMMENTS_API_URL = `https://jsonplaceholder.typicode.com/comments`;
+  const comments = await fetch(COMMENTS_API_URL);
+  const commentsData: Comment[] = await comments.json();
+
+  return {
+    props: {
+      fallback: {
+        [COMMENTS_API_URL]: commentsData,
+      },
+    },
+  };
+};
