3fc44ab21ba601f34a9dd2a7871618a6975c699d
ゆうご <yuhgo090764@gmail.com>
2022-09-11 14:46:00 +0900

    🐛 ジェネリックスの型をつけ忘れていたので修正

--- src/components/post/PostTitleByCommentId.tsx
+++ src/components/post/PostTitleByCommentId.tsx
@@ -2,6 +2,7 @@ import { FC } from "react";
 import Link from "next/link";
 import { useFetch } from "../../hooks/useFetch";
 import { API_URL } from "../../utils/const";
+import { Post } from "../../type/type";
 
 type PostByCommentIdProps = {
   id: string;
@@ -10,7 +11,7 @@ type PostByCommentIdProps = {
 export const PostTitleByCommentId: FC<PostByCommentIdProps> = (props) => {
   const { id } = props;
 
-  const { data, error, isLoading } = useFetch(
+  const { data, error, isLoading } = useFetch<Post, string | null>(
     id ? `${API_URL}/posts/${id}` : null
   );
 
